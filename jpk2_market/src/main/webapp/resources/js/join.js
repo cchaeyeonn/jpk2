fdcc6830ec715ddbab50101008f17678b49c43eb
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2022-06-29 12:38:28 +0900

    js 유효성 검사

--- jpk2_market/src/main/webapp/resources/js/join.js
+++ jpk2_market/src/main/webapp/resources/js/join.js
@@ -40,6 +40,13 @@
 	
 		$(function(){
 			
+			 var getMail = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
+             let reg = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/);
+
+			
+			
+			
+			
 			$("#member_id").focusout(function(){
 				
 				let member_id = $("#member_id").val();
@@ -107,8 +114,148 @@
 			}
             });
 
+		  // 유효성 검사
+		  // 아이디 유효성 검사
+		  if($("#member_id").val() == ""){
+           result = "아이디를 입력해주세요";
+	       $("#result_checkId").html(result).css("color", "red");
+           $("#member_id").focus();
+           return false;
+          }
+        
+          // 아이디 형식 유효성 검사
+          if(!getMail.test($("#member_id").val())){
+           result = "이메일 형식으로 입력하세요";
+	       $("#result_checkId").html(result).css("color", "red");
+           $("#member_id").focus();
+           return false;
+          }
+
+          // 비밀번호 유효성 검사
+	      if($("#member_pw").val() == ""){
+           result = "비밀번호를 입력하세요";
+	       $("#result_password1").html(result).css("color", "red");
+           $("#member_password1").focus();
+           return false;
+             }
+      
+          // 비밀번호 형식 유효성 검사( 영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자가 되는지 검사를 한다.)
+          
+          if(!reg.test($("#member_password1").val())){
+           result = "영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자가 되어야 합니다";
+	       $("#result_password1").html(result).css("color", "red");
+           $("#member_password1").focus();
+           return false;
+             }
+             
+          // 비밀번호가 아이디와 같지 않도록 하는 유효성 검사
+          if($("#member_id").val() == $("#member_password1").val()){
+             result = "아이디와 비밀번호가 같습니다";
+	       $("#result_password1").html(result).css("color", "red");
+           $("#member_password1").focus();
+                     return false;
+           }
+ 
+          // 비밀번호 확인란  공백 유효성 검사
+          if($("#member_password2").val() == ""){
+           result = "비밀번호 확인란을 입력해주세요";
+	       $("#result_password2").html(result).css("color", "red");
+           $("#member_password2").focus();
+           return false;
+             }
+	
+           // 비밀번호 확인 유효성 검사
+		   if($("#member_password1").val() != $("#member_password2").val()){
+              result = "비밀번호가 서로 다릅니다";
+	          $("#result_password2").html(result).css("color", "red");
+              $("#member_password1").val("");
+              $("#member_password2").val("");
+              $("#member_password1").focus();
+              return false;
+               }	
+			// 이름 유효성 검사
+			if($("#member_name").val() == ""){
+              result = "이름을 입력해주세요";
+	          $("#result_name").html(result).css("color", "red");
+              $("#member_name").focus();
+              return false;
+          }
+			// 이메일 유효성 검사
+			if($("#member_email").val() == ""){
+              result = "이메일을 입력해주세요";
+	          $("#result_checkEmail").html(result).css("color", "red");
+              $("#member_email").focus();
+              return false;
+          }
+			// 이메일 형식 유효성 검사
+			if(!getMail.test($("#member_email").val())){
+           result = "이메일 형식으로 입력하세요";
+	       $("#result_checkEmail").html(result).css("color", "red");
+           $("#member_email").focus();
+           return false;
+          }
+           // 핸드폰 번호 유효성 검사
+          
+			if($("#member_phone").val() == ""){
+              result = "핸드폰 번호를 입력해주세요";
+	          $("#result_phone").html(result).css("color", "red");
+              $("#member_phone").focus();
+              return false;
+          }
+		   // 주소 유효성 검사
+		    if($("#member_addr_1").val() == ""){
+              result = "주소를 입력해주세요";
+	          $("#result_addr").html(result).css("color", "red");
+              return false;
+          }
+		    if($("#member_addr_2").val() == ""){
+              result = "주소를 입력해주세요";
+	          $("#result_addr").html(result).css("color", "red");
+              return false;
+          }
+            //성별 유효성 검사
+		   	if($("input[name=gender]:radio:checked").length < 1){
+			  result = "성별을 선택하세요";
+	          $("#result_gender").html(result).css("color", "red");
+              return false;
 			
 			
+		 }
+			//생년월일이 오늘이 최대값이 되도록 설정
+			var now_utc = Date.now();
+			var timeOff = new Date().getTimezoneOffset()*60000;
+			var today = new Date(now_utc-timeOff).toISOString().split("T")[0];
+			document.getElementById("Date").setAttribute("max", today);
+			
+		   
+		    //생년월일 유효성 검사
+		     if($("#member_birth").val() == "2022-01-01"){
+              result = "생년월일을 입력해주세요";
+	          $("#result_birth").html(result).css("color", "red");
+              return false;
+          }
+			//약관 유효성 검사
+			if (!$("input:checked[Name='member_termall']").is(":checked")){
+			  result = "약관을 동의해주세요";
+	          $("#result_term").html(result).css("color", "red"); 
+		   return false;
+	      }else if (!$("input:checked[Name='member_term1']").is(":checked")){
+			  result = "약관을 동의해주세요";
+	          $("#result_term").html(result).css("color", "red"); 
+		   return false;
+	      }
+	      else if (!$("input:checked[Name='member_term2']").is(":checked")){
+			  result = "약관을 동의해주세요";
+	          $("#result_term").html(result).css("color", "red"); 
+		   return false;
+	      }
+	      else if (!$("input:checked[Name='member_term3']").is(":checked")){
+			  result = "약관을 동의해주세요";
+	          $("#result_term").html(result).css("color", "red"); 
+		   return false;
+	      }
+
+
 			
 		});
 
