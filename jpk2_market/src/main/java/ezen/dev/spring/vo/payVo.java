1053c120e9f101e994aa5b9e7fc47a61aaff8833
720 <720@DESKTOP-MKODU3L>
2022-07-07 16:40:54 +0900

    220707 풀용 커밋 + vo

---
+++ jpk2_market/src/main/java/ezen/dev/spring/vo/payVo.java
@@ -0,0 +1,66 @@
+package ezen.dev.spring.vo;
+
+public class payVo {
+	private int payidx;
+	private String pay_price;
+	private String pay_way;
+	private String pay_delyn;
+	private String delyndate;
+	private String pay_check;
+	private String pay_finedate;
+	private int oidx_op;
+	
+	public int getPayidx() {
+		return payidx;
+	}
+	public void setPayidx(int payidx) {
+		this.payidx = payidx;
+	}
+	public String getPay_price() {
+		return pay_price;
+	}
+	public void setPay_price(String pay_price) {
+		this.pay_price = pay_price;
+	}
+	public String getPay_way() {
+		return pay_way;
+	}
+	public void setPay_way(String pay_way) {
+		this.pay_way = pay_way;
+	}
+	public String getPay_delyn() {
+		return pay_delyn;
+	}
+	public void setPay_delyn(String pay_delyn) {
+		this.pay_delyn = pay_delyn;
+	}
+	public String getDelyndate() {
+		return delyndate;
+	}
+	public void setDelyndate(String delyndate) {
+		this.delyndate = delyndate;
+	}
+	public String getPay_check() {
+		return pay_check;
+	}
+	public void setPay_check(String pay_check) {
+		this.pay_check = pay_check;
+	}
+	public String getPay_finedate() {
+		return pay_finedate;
+	}
+	public void setPay_finedate(String pay_finedate) {
+		this.pay_finedate = pay_finedate;
+	}
+	public int getOidx_op() {
+		return oidx_op;
+	}
+	public void setOidx_op(int oidx_op) {
+		this.oidx_op = oidx_op;
+	}
+	
+	
+	
+	
+	
+}
--- jpk2_market/src/main/webapp/resources/js/cart.js
+++ jpk2_market/src/main/webapp/resources/js/cart.js
@@ -20,19 +20,25 @@ $(document).on('click','.del-chk',function(){
 	      
 // + - 버튼
 let number = $('#pop_out').val();
-    
+   
 $('#btn_minus').on('click',function(){
 	    number = parseInt(number)
-	    
-        $('#pop_out').attr('value',number-1);
+	    $('#pop_out').attr('value',number-1);
+	    return;
         });
+		
+		
 
 $(document).on('click','#btn_plus',function(){
 	    number = parseInt(number)
         $('#pop_out').attr('value',number+1);
 
 
+return;
+	   });
+	
 
-	   });  
+
+
 	    
   });
