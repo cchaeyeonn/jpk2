e98ecca6de0cfe81e9586b3a0ed4c660ba626066
720 <720@DESKTOP-MKODU3L>
2022-07-11 10:15:18 +0900

    220711 풀용

--- jpk2_market/src/main/java/ezen/dev/spring/controller/NoticeController.java
+++ jpk2_market/src/main/java/ezen/dev/spring/controller/NoticeController.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -42,6 +43,64 @@ private NoticeService noticeService;
 	
 	
 	
+		@GetMapping("/notice_wirte.do")
+	public String adminProductAdd() {
+		
+		return "service_center/notice_write";
+	}
+	
+	
+	/*@PostMapping("/notice_wirteProcess.do")
+	public String productAddProcess(
+			String n_category, String n_title, String n_content, String n_writedate, Integer mmidx_mn)
+					throws IllegalStateException, IOException {
+
+		
+		String realPath = request.getServletContext().getRealPath(upload_dir);
+		System.out.println("이클립스로 저장된 파일의 실제 경로: " + realPath);
+		String fullPath = realPath+p_system_filename;
+		uploadFile.transferTo(new File(fullPath));
+		
+		int result=0;
+		
+		midx_mp=Integer.parseInt(String.valueOf(session.getAttribute("midx")));
+		
+		ProductVo productVo = new ProductVo();
+		productVo.setP_name(p_name);
+		productVo.setP_price(p_price);
+		productVo.setP_unit(p_unit);
+		productVo.setP_weight(p_weight);
+		productVo.setP_delivery(p_delivery);
+		productVo.setP_package(p_package);
+		productVo.setP_allergy(p_allergy);
+		productVo.setP_limitdate(p_limitdate);
+		productVo.setP_type(p_type);
+		productVo.setP_tag(p_tag);
+		productVo.setMidx_mp(midx_mp);
+		productVo.setP_filename(p_filename);
+		productVo.setP_system_filename(p_system_filename);
+
+		result = adminService.addProduct(productVo);
+		String viewPage="admin/admin_product_add";
+
+		if(result ==1) {
+			model.addAttribute("p_name",p_name);
+			model.addAttribute("p_price",p_price);
+			model.addAttribute("p_unit",p_unit);
+			model.addAttribute("p_weight",p_weight);
+			model.addAttribute("p_delivery",p_delivery);
+			model.addAttribute("p_package",p_package);
+			model.addAttribute("p_allergy",p_allergy);
+			model.addAttribute("p_limitdate",p_limitdate);
+			model.addAttribute("p_type",p_type);
+			model.addAttribute("p_tag",p_tag);
+			model.addAttribute("midx",midx_mp);
+			model.addAttribute("p_filename",p_filename);
+			model.addAttribute("p_system_filename",p_system_filename);
+			viewPage = "admin/admin_home";
+		}
+		return viewPage;
+	}*/
 	
 	
 	
---
+++ jpk2_market/src/main/webapp/WEB-INF/views/service_center/notice_write.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>공지사항 글작성</title>
+<style>
+
+/* 	#product{
+		padding-left:10%;
+		padding-right:10%; 
+		height:1500px; 
+		padding-top:260px;
+		}
+	#product_img{
+			float:left; 
+			width:50%; 
+			height:50%;
+			}
+	#product_detail{
+			float:right; 
+			width:50%; 
+			height:50%;
+			}
+	#home_button{
+			padding-left:10%; 
+			padding-right:10%; 
+			text-align:center; 
+			float:center;
+			} */
+			
+	#notice_main_inner{
+		min-height:1040px;
+		padding-top:500px;
+	}
+			
+</style>
+</head>
+<body>
+
+    <!-- 헤더 연결 -->
+    <jsp:include page="../header.jsp"></jsp:include> 
+	
+		<div id="notice_main_inner">
+			<table><form name="notice_write_Form" action="/spring/notice_wirteProcess.do" method="post" ><!-- enctype="multipart/form-data" -->
+			<tr>
+				<td>카테고리</td>
+				<td><select name="n_category">
+					<option value="배송">배송</option>
+					<option value="기타">기타</option>
+				</select></td>
+			</tr>
+			<tr>
+			<td>제목</td>
+			<td><input type="text" name="n_title"></td>
+			</tr>
+			<tr>
+			<td>내용</td>
+			<td><input type="text" name="n_content"></td>
+			</tr>
+			</form>
+			</table>
+		</div>
+		<div id="notice_write_button"><input type="submit" value="작성하기"></div><p/>
+	
+    <!-- 푸터 연결 -->
+    <jsp:include page="../footer.jsp"></jsp:include>
+</body>
+</html>
